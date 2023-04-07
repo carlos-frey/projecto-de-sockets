a75110cb4586adeaff05882ce0b94500b34691ce
Carlos Frey <chsf@cin.ufpe.br>
2023-04-07 13:25:18 -0300

    A base do servidor web foi finalizada

---
+++ web-server/utils.py
@@ -0,0 +1,9 @@
+def convert_request_to_dict(request):
+    lines = request.split('\r\n')
+    headers = {}
+    for line in lines[1:]:
+        if line:
+            header, value = line.split(': ')
+            headers[header] = value
+    
+    return headers
